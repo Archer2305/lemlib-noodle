2c7c76476e1d6d9b64bc24ca95f81248494ce553
taneesh-m <taneesh.m15@gmail.com>
2024-01-20 16:44:03 -0800

    aaa

--- src/main.cpp
+++ src/main.cpp
@@ -97,33 +97,28 @@ void competition_initialize() {}
 // this needs to be put outside a function
 ASSET(example_txt); // '.' replaced with "_" to make c++ happy
 
-void autonomous() {
-    // example movement: Move to x: 20 and y: 15, and face heading 90. Timeout set to 4000 ms
-    //chassis.moveToPoint(0, 24, 100000);
-    chassis.moveToPose(24, 24, 90, 4000);
-    // example movement: Move to x: 0 and y: 0 and face heading 270, going backwards. Timeout set to 4000ms
-    //chassis.turnTo(45, -45, 10000, true, 88);
-    #if 0
-    chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-    // cancel the movement after it has travelled 10 inches
-    chassis.waitUntil(10);
-    chassis.cancelMotion();
-    // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnTo(45, -45, 1000, true, 60);
-    // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has travelled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
-    // wait until the movement is done
+void flipout() {
+    liftMotor.move_voltage(12000);
+    pros::delay(120);
+    liftMotor.move_voltage(0);
+}
+
+
+void elims() {
+    flipout();
+    chassis.moveToPoint(0, 16, 1000);
+    chassis.moveToPose(-8, 38, 93, 1000);
+    intakeMotor.move_velocity(-600);
+    pros::delay(640);
+    intakeMotor.move_velocity(0);
+
+    chassis.moveToPoint(12, 38, 2000, {.forwards=false});
+    wings.set_state(1);
     chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
-    #endif
+    
+}
+void autonomous() {
+
 }
 
 void opcontrol() {
